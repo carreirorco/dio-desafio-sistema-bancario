4d1f7057677acf403fe4004b1f593b8284f237ad
Rafael Carreiro <carreirorco@gmail.com>
2025-08-29 20:36:49 -0300

    feat: add withdrawal option

--- app.py
+++ app.py
@@ -27,8 +27,29 @@ while True:
             print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "s":
-        print("Você escolheu a opção de saque.")
-        pass
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques diários excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor: .2f}\n"
+            numero_saques += 1
+            print("Você sacou R$ {:.2f}".format(valor))
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "e":
         print("Você escolheu a opção de extrato.")
