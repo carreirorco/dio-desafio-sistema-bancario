b956b4cf37703c1b8b88fff1a0d4cbb4dbd588b9
Rafael Carreiro <carreirorco@gmail.com>
2025-08-29 20:28:25 -0300

    feat: add app.py with initial structure

---
+++ app.py
@@ -0,0 +1,38 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        print("Você escolheu a opção de depósito.")
+        pass
+
+    elif opcao == "s":
+        print("Você escolheu a opção de saque.")
+        pass
+
+    elif opcao == "e":
+        print("Você escolheu a opção de extrato.")
+        pass
+
+    elif opcao == "q":
+        print("Você escolheu a opção de sair.")
+        pass
+        break
+    
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("Encerrando o programa...")
